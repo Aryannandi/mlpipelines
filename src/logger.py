694ed7124a8a4497139336c1cde4c889433b74e3
Aryannandi <aryannandi73@gmail.com>
2025-02-08 18:37:04 +0530

    model training and model pipeline completed

--- src/logger.py
+++ src/logger.py
@@ -6,9 +6,11 @@ from datetime import datetime
 
 log_file = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 
-log_path = os.path.join(os.getcwd(), "logs", log_file)
+log_path = os.path.join(os.getcwd(), "logs")
 
-log_file_path = os.makedirs(log_path, exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
+
+log_file_path = os.path.join(log_path, log_file)
 
 logging.basicConfig(
     filename=log_file_path,
